b57897facc9ff4353e251ab253d75fb0c69d8cf8
Tony Liang <powerstargreen@gmail.com>
2019-10-24 15:53:22 -0500

    Saved on proj0

---
+++ YourStore.Library/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace YourStore.Library
+{
+    public class Class1
+    {
+    }
+}
---
+++ YourStore.Library/Model/IProduct.cs
@@ -0,0 +1,12 @@
+﻿namespace YourStore.Library.Model
+{
+    public interface IProduct
+    {
+        float Cost { get; set; }
+        int ID { get; set; }
+        string Name { get; set; }
+
+        void Refill();
+        bool Restockable();
+    }
+}
